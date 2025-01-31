ad6e67041c8a3958a9b0eda1fcc09544ad26f663
manish-atlantis <techgeekmanish@gmail.com>
2025-02-01 01:07:11 +0530

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -84,7 +84,7 @@ const ContactUs = () => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
               <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
             </svg>
-            <span className="text-lg font-medium text-black">Manish Gupta, Akshaya, RVITM, Bengaluru-560068</span>
+            <span className="text-lg font-medium text-black">Akshaya, RVITM, Bengaluru-560068</span>
           </div>
         </div>
         
